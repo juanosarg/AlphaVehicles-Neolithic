41cd8c16431f60a56a45a6f4af57d76e3cdc1586
SargBjornson <sarg.auxiliar@gmail.com>
2023-08-15 21:33:04 +0200

    v0.1

---
+++ 1.4/Source/AlphaVehicles/AlphaVehicles/Harmony/HarmonyInstance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using System.Reflection;
+
+namespace AlphaVehicles
+{
+
+    //Setting the Harmony instance
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.alphavehicles");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+
+}
---
+++ 1.4/Source/AlphaVehicles/AlphaVehicles/Projectiles/Projectile_Distortion.cs
@@ -0,0 +1,33 @@
+﻿
+using Verse;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Verse.Sound;
+using UnityEngine;
+using System.Collections;
+
+namespace AlphaVehicles
+{
+    public class Projectile_Distortion : Projectile_Explosive
+    {
+
+
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (this.IsHashIntervalTick(10))
+            {
+                try
+                {
+                    FleckMaker.AttachedOverlay(this, FleckDefOf.PsycastAreaEffect, Vector3.zero, 3f, -1f);
+                }
+                catch (Exception) { }
+
+            }
+        }
+    }
+}
