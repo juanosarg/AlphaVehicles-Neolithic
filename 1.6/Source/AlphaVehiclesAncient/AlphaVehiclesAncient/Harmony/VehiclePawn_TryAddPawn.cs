ae4604c73bd628e2dd3a7d1756ec19386f36332e
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-07-14 18:28:13 +0200

    v3.0

---
+++ 1.6/Source/AlphaVehiclesAncient/AlphaVehiclesAncient/Harmony/VehiclePawn_TryAddPawn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Vehicles;
+
+
+
+namespace AlphaVehicles
+{
+
+
+
+    [HarmonyPatch(typeof(VehiclePawn))]
+    [HarmonyPatch("TryAddPawn")]
+    [HarmonyPatch(new Type[] { typeof(Pawn), typeof(VehicleRoleHandler) })]
+    public static class AlphaVehicles_VehiclePawn_TryAddPawn_Patch
+    {
+
+        public static void Postfix(Pawn pawn, VehiclePawn __instance)
+        {
+
+            if (__instance.TryGetComp<CompAddHediffToVehiclePassenger>() is CompAddHediffToVehiclePassenger comp && comp != null)
+            {
+                foreach (VehicleRoleHandler handler in __instance.handlers)
+                {
+                    foreach (HediffToRoles hedifftoroles in comp.Props.hediffsToAdd)
+                    {
+
+                        if (handler.role.key == hedifftoroles.role && handler.thingOwner.Contains(pawn))
+                        {
+                            pawn.health.AddHediff(hedifftoroles.hediff);
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+
+
+}
